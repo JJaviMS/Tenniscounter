6562376ce8e8ff12d416c0c79e891cd88be41662
JJaviMS <javi.mateosegura@gmail.com>
2017-03-01 00:15:05 +0100

    Commit

--- app/src/main/java/com/example/android/tenniscounter/Counter.java
+++ app/src/main/java/com/example/android/tenniscounter/Counter.java
@@ -1,13 +1,25 @@
 package com.example.android.tenniscounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class Counter extends AppCompatActivity {
+    String s = getIntent().getStringExtra("nameA");
+    String q = getIntent().getStringExtra("nameB");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+        TextView nameLocal = (TextView) findViewById(R.id.counterNameA);
+        TextView nameVisitor = (TextView) findViewById(R.id.counterNameB);
+        nameLocal.setText(s);
+        nameVisitor.setText(q);
+
+
+
+
     }
 }
