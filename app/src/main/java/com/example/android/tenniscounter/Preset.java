1b2c301948249da6585d1fb86c8869ff29f72408
JJaviMS <javi.mateosegura@gmail.com>
2017-02-28 22:54:21 +0100

    Add new class and make the intent

--- app/src/main/java/com/example/android/tenniscounter/Preset.java
+++ app/src/main/java/com/example/android/tenniscounter/Preset.java
@@ -1,7 +1,11 @@
 package com.example.android.tenniscounter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Preset extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class Preset extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preset);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent scoreIntent = new Intent (Preset.this,Counter.class);
+                EditText playerA = (EditText) findViewById(R.id.localPlayer);
+                EditText playerB = (EditText) findViewById(R.id.visitorPlayer);
+                String namePlayerB = playerB.getText().toString();
+                String namePlayerA = playerA.getText().toString();
+                scoreIntent.putExtra("nameA",namePlayerA);
+                scoreIntent.putExtra("nameB",namePlayerB);
+                startActivity(scoreIntent);
+            }
+        });
     }
+
+
+
+
 }
