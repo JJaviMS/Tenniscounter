8349972895cbacf70e90d424fb5f9902a9fa4329
JJaviMS <javi.mateosegura@gmail.com>
2017-03-01 13:39:37 +0100

    Commit

--- app/src/main/java/com/example/android/tenniscounter/Counter.java
+++ app/src/main/java/com/example/android/tenniscounter/Counter.java
@@ -2,9 +2,15 @@ package com.example.android.tenniscounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class Counter extends AppCompatActivity {
+    int playerAscore = 0;
+    int playerBscore = 0;
+    int numberOfsets;
+    int playerAset=0;
+    int playerBset = 0;
 
 
 
@@ -14,13 +20,48 @@ public class Counter extends AppCompatActivity {
         setContentView(R.layout.activity_counter);
         String s = getIntent().getStringExtra("nameA");
         String q = getIntent().getStringExtra("nameB");
+        numberOfsets = getIntent().getIntExtra("numberOfSets",1);
         TextView nameLocal = (TextView) findViewById(R.id.counterNameA);
         TextView nameVisitor = (TextView) findViewById(R.id.counterNameB);
         if (!s.equals(new String()))
         nameLocal.setText(s);
         if(!q.equals(new String()))
         nameVisitor.setText(q);
-        int playerAscore = 0;
-        int playerBscore = 0;
+
     }
+    public void scoreA (View view){
+        playerAscore++;
+        displayA(playerAscore);
+    }
+    public void displayA (int score) {
+        TextView scoreA = (TextView) findViewById(R.id.actualAPoints);
+        if (score == 0)
+            scoreA.setText("0");
+        else if (score == 1)
+            scoreA.setText("15");
+        else if (score == 2)
+            scoreA.setText("30");
+        else
+            scoreA.setText("40");
+    }
+    public void scoreB (View view){
+        playerBscore++;
+        displayB(playerBscore);
+    }
+    public void displayB (int score) {
+        TextView scoreB = (TextView) findViewById(R.id.actualBPoints);
+        if (score == 0)
+            scoreB.setText("0");
+        else if (score == 1)
+            scoreB.setText("15");
+        else if (score == 2)
+            scoreB.setText("30");
+        else
+            scoreB.setText("40");
+    }
+
+
+
+
+
 }
